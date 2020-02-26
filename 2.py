27c006ce10c699c185da9d45108bdb519893e348
Arvid <arvid@wartenberg.se>
2020-02-26 15:10:41 +0100

    Ny kod för kulan

--- 2.py
+++ 2.py
@@ -40,15 +40,27 @@ print('eigs2' + str(simplify(eigs[0].subs([(S,sol_bio[0]),(I,sol_bio[1])]))))
 '''
 
 def find_next_event(b_n, d_n, dt):
+    t = 0
+    win = -1
+    while True:
+        rand_1 = np.random.uniform(0, 1)
+        rand_2 = np.random.uniform(0, 1)
+        if rand_1 < d_n*dt:
+            return t, 0
+        elif rand_2 < b_n*dt:
+            return t, 1
+        t += dt
+'''
     p_b = b_n * dt
     p_d = d_n * dt
-    t_b = np.random.negative_binomial(n=1, p=p_b)
-    t_d = np.random.negative_binomial(n=1, p=p_d)
-    return min(t_b,t_d)*dt, int(t_b>=t_d) # Returns 1 if recovery happens first. 0 else.
+    t_b = np.random.negative_binomial(n=1, p=p_b)*dt
+    t_d = np.random.negative_binomial(n=1, p=p_d)*dt
+    return min(t_b,t_d), int(t_b>=t_d) # Returns 1 if recovery happens first. 0 else.
+'''
 
 b_n, d_n =  [0.1, 1, 10], [0.2, 2, 5] # b_n: new infection, d_n: recovery
-dt = 0.001
-n_events = 300000
+dt = 0.001 # big time steps cause problems in iterate method for last case as both should pass often but only first does
+n_events = 10000
 
 events = np.zeros(shape=[n_events, 6])
 for i in range(n_events):
@@ -74,25 +86,40 @@ rec_times_3, inf_times_3 = find_event_distance_time(events[:,4:6])
 bins_1 = np.linspace(0, max(max(rec_times_1), max(inf_times_1)), 100)
 bins_2 = np.linspace(0, max(max(rec_times_2), max(inf_times_2)), 100)
 bins_3 = np.linspace(0, max(max(rec_times_3), max(inf_times_3)), 100)
+line_1_b = b_n[0]*np.exp(-b_n[0]*bins_1)
+line_1_d = d_n[0]*np.exp(-d_n[0]*bins_1)
+line_2_b = b_n[1]*np.exp(-b_n[1]*bins_2)
+line_2_d = d_n[1]*np.exp(-d_n[1]*bins_2)
+line_3_b = b_n[2]*np.exp(-b_n[2]*bins_3)
+line_3_d = d_n[2]*np.exp(-d_n[2]*bins_3)
 
 plt.figure()
 
 plt.subplot(3,1,1)
 plt.title('Norm. log. Hist for time between inf/rec', fontsize=17)
-plt.hist(rec_times_1, bins=bins_1, color='b', alpha=.5, label='$t_{rec}$, $b_n=.1, d_n=.2$', density=True, log=True)
-plt.hist(inf_times_1, bins=bins_1, color='r', alpha=.5, label='$t_{inf}$, $b_n=.1, d_n=.2$', density=True, log=True)
+plt.hist(rec_times_1, bins=bins_1, color='r', alpha=.5, label='$t_{rec}$, $b_n=.1, d_n=.2$', density=True)#, log=True)
+plt.hist(inf_times_1, bins=bins_1, color='b', alpha=.5, label='$t_{inf}$, $b_n=.1, d_n=.2$', density=True)#, log=True)
+plt.plot(bins_1, line_1_b, color='b', linewidth=3)
+plt.plot(bins_1, line_1_d, color='r', linewidth=3)
+plt.semilogy()
 plt.ylabel('log(density)')
 plt.legend(fontsize=10)
 
 plt.subplot(3,1,2)
-plt.hist(rec_times_2, bins=bins_2, color='b', alpha=.5, label='$t_{rec}$, $b_n=1, d_n=2$', density=True, log=True)
-plt.hist(inf_times_2, bins=bins_2, color='r', alpha=.5, label='$t_{inf}$, $b_n=1, d_n=2$', density=True, log=True)
+plt.hist(rec_times_2, bins=bins_2, color='r', alpha=.5, label='$t_{rec}$, $b_n=1, d_n=2$', density=True)#, log=True)
+plt.hist(inf_times_2, bins=bins_2, color='b', alpha=.5, label='$t_{inf}$, $b_n=1, d_n=2$', density=True)#, log=True)
+plt.plot(bins_2, line_2_b, color='b', linewidth=3)
+plt.plot(bins_2, line_2_d, color='r', linewidth=3)
+plt.semilogy()
 plt.ylabel('log(density)')
 plt.legend(fontsize=10)
 
 plt.subplot(3,1,3)
-plt.hist(rec_times_3, bins=bins_3, color='b', alpha=.5, label='$t_{rec}$, $b_n=10, d_n=5$', density=True, log=True)
-plt.hist(inf_times_3, bins=bins_3, color='r', alpha=.5, label='$t_{inf}$, $b_n=10, d_n=5$', density=True, log=True)
+plt.hist(rec_times_3, bins=bins_3, color='r', alpha=.5, label='$t_{rec}$, $b_n=10, d_n=5$', density=True)#, log=True)
+plt.hist(inf_times_3, bins=bins_3, color='b', alpha=.5, label='$t_{inf}$, $b_n=10, d_n=5$', density=True)#, log=True)
+plt.plot(bins_3, line_3_b, color='b', linewidth=3)
+plt.plot(bins_3, line_3_d, color='r', linewidth=3)
+plt.semilogy()
 plt.xlabel('Time')
 plt.ylabel('log(density)')
 plt.legend(fontsize=10)
--- 2_d.py
+++ 2_d.py
@@ -6,28 +6,57 @@ import seaborn as sns
 
 
 def find_next_event(alpha, beta, N, n, dt):
-    p_b = alpha * n * (1 - n/N) * dt
-    p_d = beta*n*dt
-    t_b = np.random.negative_binomial(n=1, p=p_b)
-    t_d = np.random.negative_binomial(n=1, p=p_d)
-    return min(t_b,t_d)*dt, int(t_b>=t_d) # Returns 1 if recovery happens first. 0 else.
+    p_d = beta*n
+    t_d = np.random.exponential(scale = 1/p_d)#*dt
+    if n != N:
+        p_b = alpha * n * (1 - n / N)#*dt
+        #t_b = np.random.negative_binomial(n=1, p=p_b)*dt
+        t_b = np.random.exponential(scale = 1/p_b)#*dt
+        if t_b > t_d:
+            return t_d, -1
+        return t_b, 1
+    return t_d, -1
 
-alpha, beta = 2, 1
-N = 20
-dt = 0.01
-n_samples = 100
-n_realizations = 10
-# TODO remove first sample since it is always the same, ruins statistics
-cum_realizations = np.zeros((n_realizations, n_samples))
-for r in range(n_realizations):
+alpha, beta = .012, .01
+N = 100
+dt = 0.001
+T_ext = np.exp(N*(np.log(alpha/beta)-(1-beta/alpha)))#/dt
+max_samples = 100000
+n_realizations = 1000
+realizations = []
+r = 0
+while r < n_realizations:
     n_0 = N * (1 - beta / alpha)
-    n_t = np.zeros(n_samples)
-    n_t[-1] = n_0
-    for s in range(n_samples):
-        t_diff, event = find_next_event(alpha, beta, N, n_t[s-1], dt)
-        n_t[s] = n_t[s-1] + event - int(event==0)
-    plt.plot(n_t)
-    #cum_realizations[r,:] = n_t
-plt.show()
-plt.plot(cum_realizations)
+    n_t = [n_0]
+    t_r = [0]
+    sample = 0
+    while sample < max_samples and n_t[-1] >= 1:
+        if n_t[-1] == 0:
+            break
+        delta_t, event = find_next_event(alpha, beta, N, n_t[-1], dt)
+        n_t.append(n_t[-1] + event)
+        t_r.append(t_r[-1] + delta_t)
+        sample += 1
+    n_t = np.array(n_t)
+    t_r = np.array(t_r)
+    plt.plot(t_r, n_t)
+    realizations.append(np.vstack((t_r, n_t)).T)
+    r += 1
+
+n_t_data = np.array([])
+T_ext_arr = np.array([])
+for realization in realizations:
+    n_t_data = np.hstack((n_t_data, realization[:,1]))
+    T_ext_arr = np.hstack((T_ext_arr, realization[-1,0]))
+
+T_ext_exp = np.average(T_ext_arr)
+
+plt.figure()
+plt.hist(n_t_data, bins = 55)
+plt.figure()
+plt.hist(T_ext_arr, bins=50)
+plt.axvline(T_ext_exp, linewidth=3, color='r', label='Numerical $\langle T_{ext} \\rangle$')
+plt.axvline(T_ext, linewidth=3, color='m', linestyle='--', label='Theoretical $\langle T_{ext} \\rangle$')
+plt.title(str(T_ext))
+plt.legend()
 plt.show()
